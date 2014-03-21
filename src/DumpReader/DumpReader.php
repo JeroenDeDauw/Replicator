38dfc8e768a07559f1a2366dc38e2d7705478584
jeroendedauw <jeroendedauw@gmail.com>
2014-03-22 00:01:08 +0100

    Implemented dump walking based on XMLReader

--- src/DumpReader/DumpReader.php
+++ src/DumpReader/DumpReader.php
@@ -2,10 +2,122 @@
 
 namespace Wikibase\DumpReader;
 
+use DOMNode;
+use XMLReader;
+
 /**
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 class DumpReader {
+
+	/**
+	 * @var XMLReader
+	 */
+	private $xmlReader;
+
+	public function __construct( $dumpFile ) {
+		$this->xmlReader = new XMLReader();
+		$this->xmlReader->open( $dumpFile );
+	}
+
+	public function __destruct() {
+		$this->xmlReader->close();
+	}
+
+	public function nextEntityJson() {
+		$revisionNode = $this->nextRevisionNode();
+
+		while ( !$revisionNode->isItem() ) {
+			$revisionNode = $this->nextRevisionNode();
+		}
+
+		return $revisionNode->getItemJson();
+	}
+
+	private function nextRevisionNode() {
+		while ( !$this->isPageNode() ) {
+			$this->xmlReader->read();
+		}
+
+		$pageNode = new PageNode( $this->xmlReader->expand() );
+		return $pageNode->getRevisionNode();
+	}
+
+	private function isPageNode() {
+		return $this->xmlReader->nodeType === XMLReader::ELEMENT && $this->xmlReader->name === 'page';
+	}
+
+}
+
+class PageNode {
+
+	private $DOMNode;
+
+	public function __construct( DOMNode $pageNode ) {
+		$this->DOMNode = $pageNode;
+	}
+
+	public function getRevisionNode() {
+		foreach ( $this->DOMNode->childNodes as $childNode ) {
+
+			if ( $this->isRevisionNode( $childNode ) ) {
+				return new RevisionNode( $childNode );
+			}
+		}
+
+		throw new \RuntimeException( 'No revision node found' );
+	}
+
+	private function isRevisionNode( DOMNode $node ) {
+		return $node->nodeType === XMLReader::ELEMENT && $node->nodeName === 'revision';
+	}
+
+}
+
+class RevisionNode {
+
+	private $isItem;
+	private $text;
+
+	public function __construct( DOMNode $pageNode ) {
+		$isItem = false;
+
+		foreach ( $pageNode->childNodes as $childNode ) {
+			if ( $this->isModelNode( $childNode ) ) {
+				$isItem = $this->isItemModel( $childNode );
+			}
+
+			if ( $this->isTextNode( $childNode ) ) {
+				$this->extractJson( $childNode );
+			}
+		}
+
+		$this->isItem = $isItem;
+	}
+
+	public function isItem() {
+		return $this->isItem;
+	}
+
+	private function isModelNode( DOMNode $node ) {
+		return $node->nodeType === XMLReader::ELEMENT && $node->nodeName === 'model';
+	}
+
+	private function isItemModel( DOMNode $node ) {
+		return $node->textContent === 'wikibase-item';
+	}
+
+	private function isTextNode( DOMNode $node ) {
+		return $node->nodeType === XMLReader::ELEMENT && $node->nodeName === 'text';
+	}
+
+	private function extractJson( DOMNode $node ) {
+		$this->text = $node->textContent;
+	}
+
+	public function getItemJson() {
+		return $this->text;
+	}
 
 }
--- tests/integration/DumpReader/DumpReaderTest.php
+++ tests/integration/DumpReader/DumpReaderTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Wikibase\DumpReader;
 
+use Wikibase\DumpReader\DumpReader;
+
 /**
  * @covers Wikibase\DumpReader\DumpReader
  *
@@ -12,6 +14,26 @@ class DumpReaderTest extends \PHPUnit_Framework_TestCase {
 
 	public function testCanLoadClass() {
 		$this->assertTrue( class_exists( 'Wikibase\DumpReader\DumpReader' ) );
+	}
+
+	/**
+	 * @var DumpReader
+	 */
+	private $reader;
+
+	public function setUp() {
+		$this->reader = new DumpReader( __DIR__ . '/../../data/one-item.xml' );
+	}
+
+	public function testReadEntity() {
+		$entityJson = $this->reader->nextEntityJson();
+
+		$this->assertInternalType( 'string', $entityJson );
+
+		$entityArray = json_decode( $entityJson, true );
+		$this->assertInternalType( 'array', $entityArray );
+
+		$this->assertArrayHasKey( 'entity', $entityArray );
 	}
 
 }
