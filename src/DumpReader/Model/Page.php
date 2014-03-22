6ea35501310fb280d96bef9a366f132367bd4540
jeroendedauw <jeroendedauw@gmail.com>
2014-03-23 00:08:09 +0100

    Add Page and Revision classes

---
+++ src/DumpReader/Model/Page.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Wikibase\DumpReader\Model;
+
+/**
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class Page {
+
+	private $id;
+	private $title;
+	private $namespace;
+	private $revision;
+
+	public function __construct( $id, $title, $namespace, Revision $revision ) {
+		$this->id = $id;
+		$this->title = $title;
+		$this->namespace = $namespace;
+		$this->revision = $revision;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getNamespace() {
+		return $this->namespace;
+	}
+
+	/**
+	 * @return Revision
+	 */
+	public function getRevision() {
+		return $this->revision;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+
+}
---
+++ src/DumpReader/Model/Revision.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Wikibase\DumpReader\Model;
+
+/**
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class Revision {
+
+	private $id;
+	private $model;
+	private $format;
+	private $text;
+	private $timeStamp;
+
+	public function __construct( $id, $model, $format, $text, $timeStamp ) {
+		$this->id = $id;
+		$this->model = $model;
+		$this->format = $format;
+		$this->text = $text;
+		$this->timeStamp = $timeStamp;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getFormat() {
+		return $this->format;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getModel() {
+		return $this->model;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getText() {
+		return $this->text;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTimeStamp() {
+		return $this->timeStamp;
+	}
+
+}
