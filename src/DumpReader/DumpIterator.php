f74fa492e93d31a19de3ec8c9cdbf212be3e70bd
jeroendedauw <jeroendedauw@gmail.com>
2014-03-22 23:49:00 +0100

    Add doc tags

--- src/DumpReader/DumpIterator.php
+++ src/DumpReader/DumpIterator.php
@@ -23,12 +23,15 @@ class DumpIterator implements Iterator {
 	/**
 	 * @var int
 	 */
-	private $key;
+	private $key = 0;
 
 	public function __construct( DumpReader $reader ) {
 		$this->reader = $reader;
 	}
 
+	/**
+	 * @return null|string
+	 */
 	public function current() {
 		return $this->current;
 	}
@@ -38,10 +41,16 @@ class DumpIterator implements Iterator {
 		$this->key++;
 	}
 
+	/**
+	 * @return int
+	 */
 	public function key() {
 		return $this->key;
 	}
 
+	/**
+	 * @return bool
+	 */
 	public function valid() {
 		return $this->current !== null;
 	}
