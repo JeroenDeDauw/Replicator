6c67c3642d4696dcc48eaef1de7b5a967b15e67f
jeroendedauw <jeroendedauw@gmail.com>
2014-03-24 00:30:32 +0100

    Add command to run the tests to the CLI interface to verify it works

--- src/Replicator/Commands/RunTestsCommand.php
+++ src/Replicator/Commands/RunTestsCommand.php
@@ -3,11 +3,21 @@
 namespace QueryR\Replicator\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class RunTestsCommand extends Command {
 
 	protected function configure() {
-		$this->setName( 'run-tests' );
+		$this->setName( 'test' );
+		$this->setDescription( 'Run the PHPUnit tests' );
+	}
+
+	protected function execute( InputInterface $input, OutputInterface $output ) {
+		$pwd = getcwd();
+		chdir( __DIR__ . '/../../..' );
+		passthru( 'php vendor/bin/phpunit' );
+		chdir( $pwd );
 	}
 
 }
