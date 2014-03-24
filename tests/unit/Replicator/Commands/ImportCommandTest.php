024fed753b2d7bcda28316b058e06da7e26a26f7
jeroendedauw <jeroendedauw@gmail.com>
2014-03-24 22:04:36 +0100

    Add ImportCommandTest

---
+++ tests/unit/Replicator/Commands/ImportCommandTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\QueryR\Replicator\Commands;
+
+use QueryR\Replicator\Commands\ImportCommand;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @covers QueryR\Replicator\Commands\ImportCommand
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class ImportCommandTest extends \PHPUnit_Framework_TestCase {
+
+	public function testEntityIdInOutput() {
+		$command = new ImportCommand();
+		$commandTester = new CommandTester( $command );
+
+		$commandTester->execute( array(
+			'file' => 'tests/data/simple/one-item.xml'
+		) );
+
+		$this->assertRegExp( '/Q15831780/', $commandTester->getDisplay() );
+	}
+
+}
