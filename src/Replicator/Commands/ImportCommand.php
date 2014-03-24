886d769ae8a63658d97c8680c2a33aa3511d6068
jeroendedauw <jeroendedauw@gmail.com>
2014-03-24 21:56:52 +0100

    Rename Factory to ReaderFactory to avoid import collisions

--- src/Replicator/Commands/ImportCommand.php
+++ src/Replicator/Commands/ImportCommand.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Wikibase\Dump\Reader\Factory;
+use Wikibase\Dump\Reader\ReaderFactory;
 use Wikibase\Dump\Page;
 
 class ImportCommand extends Command {
@@ -23,7 +23,7 @@ class ImportCommand extends Command {
 	}
 
 	protected function execute( InputInterface $input, OutputInterface $output ) {
-		$dumpReaderFactory = new Factory();
+		$dumpReaderFactory = new ReaderFactory();
 
 		$dumpReader = $dumpReaderFactory->newDumpReaderForFile( $input->getArgument( 'file' ) );
 
