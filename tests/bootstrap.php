b3dc109c44b2c9da2dc44ac99c976458c2230f09
jeroendedauw <jeroendedauw@gmail.com>
2014-04-21 10:00:26 +0200

    Move out the dump store

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,4 +13,6 @@ if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
 }
 
-require_once( __DIR__ . '/../vendor/autoload.php' );
+$loader = require_once( __DIR__ . '/../vendor/autoload.php' );
+
+$loader->addPsr4( 'Tests\\QueryR\\Replicator\\Fixtures\\', 'tests/fixtures' );
--- tests/fixtures/TestFixtureFactory.php
+++ tests/fixtures/TestFixtureFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Fixtures;
+namespace Tests\QueryR\Replicator\Fixtures;
 
 use PDO;
 
