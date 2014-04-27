8b890fb85a695455123e6610ab157238a6bff71a
jeroendedauw <jeroendedauw@gmail.com>
2014-04-27 17:42:57 +0200

    Make console output nicer

--- src/Replicator/Commands/Installer/UninstallCommandExecutor.php
+++ src/Replicator/Commands/Installer/UninstallCommandExecutor.php
@@ -2,6 +2,7 @@
 
 namespace Queryr\Replicator\Commands\Installer;
 
+use Queryr\Replicator\Commands\ProgressTrait;
 use Queryr\Replicator\ConfigFile;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
