ff4c073405077dd6ac0bf8155d242dc2f45c60e9
jeroendedauw <jeroendedauw@gmail.com>
2014-09-06 21:04:35 +0200

    Remove not needed subdirs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,8 +8,4 @@ if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
 	die( 'You need to install this package with Composer before you can run the tests' );
 }
 
-$loader = require_once( __DIR__ . '/../vendor/autoload.php' );
-
-$loader->addPsr4( 'Tests\\Queryr\\Replicator\\', 'tests/' );
-
-unset( $loader );
+require_once( __DIR__ . '/../vendor/autoload.php' );
